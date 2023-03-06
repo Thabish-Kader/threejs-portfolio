3f50e0dd5184860b7400d3ea30c6843bc1210b71
Thabish <kadertabish2@gmail.com>
2023-03-06 14:09:49 +0400

    hero-section
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ const App = () => {
 	return (
 		<BrowserRouter>
 			<div className="relative z-0 bg-primary">
+				{/* TODO: Change background img */}
 				<div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
 					<Navbar />
 					<Hero />
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { motion } from "framer-motion";
+import { styles } from "../styles";
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+	return (
+		<section className="relative w-full h-screen mx-auto">
+			<div
+				className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+			>
+				<div className="flex flex-col justify-center items-center mt-5">
+					{/* dot */}
+					<div className="w-5 h-5 rounded-full bg-[#915eff]" />
+					{/* line */}
+					<div className="w-1 sm:h-80 h-40 violet-gradient" />
+				</div>
+				{/* hello message */}
+				<div>
+					<h1 className={`${styles.heroHeadText}`}>
+						Hi, I'm <span className="text-[#915eff]">thabish</span>
+					</h1>
+					<p className={`${styles.heroSubText} mt-2 text-white-100`}>
+						{/* TODO: Change the message */}I create stunning 3D UI
+						for web applications
+					</p>
+				</div>
+			</div>
+		</section>
+	);
+};
 
-export default Hero
+export default Hero;
