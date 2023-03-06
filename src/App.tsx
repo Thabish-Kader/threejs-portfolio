dbb46ae8f57efe416d2261f5db4cca66ce0f9a4f
Thabish <kadertabish2@gmail.com>
2023-03-06 12:03:36 +0400

    initial-config
    Signed-off-by: Thabish <kadertabish2@gmail.com>

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Works from "./components/Works";
+import Tech from "./components/Tech";
+import Feedbacks from "./components/Feedbacks";
+import Contact from "./components/Contact";
+import Stars from "./components/canvas/Stars";
+const App = () => {
+	return (
+		<BrowserRouter>
+			<div className="relative z-0 bg-primary">
+				<div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+					<Navbar />
+					<Hero />
+				</div>
+				<About />
+				<Experience />
+				<Tech />
+				<Works />
+				<Feedbacks />
+				<div className="relative z-0">
+					<Contact />
+					<Stars />
+				</div>
+			</div>
+		</BrowserRouter>
+	);
+};
+
+export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+const Navbar = () => {
+	return <div>Navbar</div>;
+};
+
+export default Navbar;
