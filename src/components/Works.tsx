c01f1d47fd42a635d6573ecfcb62fc7de1df85f2
Thabish <kadertabish2@gmail.com>
2023-03-07 15:37:32 +0400

    work
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import Tilt from "react-parallax-tilt";
+import { motion } from "framer-motion";
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc/SectionWrapper";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const Works = () => {
-  return (
-    <div>Works</div>
-  )
-}
+	return (
+		<>
+			<motion.div variants={textVariant(1.5)}>
+				<p className="sectionSubText">The Projects I have worked on</p>
+				<h2 className="sectionHeadText">Projects</h2>
+			</motion.div>
 
-export default Works
+			<div className="w-full flex">
+				<motion.p
+					variants={fadeIn("", "", 0.1, 0.1)}
+					className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+				></motion.p>
+			</div>
+		</>
+	);
+};
+
+export default SectionWrapper(Works, "");
