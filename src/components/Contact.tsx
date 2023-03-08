1702b1ea485eb49f500fa35cc92302eac3b57aa7
Thabish <kadertabish2@gmail.com>
2023-03-08 12:24:01 +0400

    modal-added
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,14 +1,20 @@
-import React, { FormEvent, useRef, useState } from "react";
+import { FormEvent, useRef, useState, Fragment } from "react";
 import { SectionWrapper } from "../hoc/SectionWrapper";
 import { motion } from "framer-motion";
 import { slideIn } from "../utils/motion";
 import EarthCanvas from "./canvas/EarthCanvas";
 import emailjs from "@emailjs/browser";
+import { Dialog, Transition } from "@headlessui/react";
 
 const Contact = () => {
 	const formRef = useRef<HTMLFormElement>(null);
 	const [form, setForm] = useState({ name: "", email: "", message: "" });
 	const [loading, setLoading] = useState(false);
+	let [isOpen, setIsOpen] = useState(false);
+
+	function closeModal() {
+		setIsOpen(false);
+	}
 
 	const handleSubmit = (e: FormEvent) => {
 		e.preventDefault();
@@ -28,8 +34,8 @@ const Contact = () => {
 			)
 			.then(
 				() => {
-					setLoading(false),
-						alert("I will get back to you as soon as possible");
+					setLoading(false);
+					setIsOpen(true);
 					setForm({ name: "", email: "", message: "" });
 				},
 				(error) => {
@@ -118,6 +124,73 @@ const Contact = () => {
 			>
 				<EarthCanvas />
 			</motion.div>
+
+			{/* Pop up Modal */}
+			<Transition appear show={isOpen} as={Fragment}>
+				<Dialog as="div" className="relative z-10" onClose={closeModal}>
+					<Transition.Child
+						as={Fragment}
+						enter="ease-out duration-300"
+						enterFrom="opacity-0"
+						enterTo="opacity-100"
+						leave="ease-in duration-200"
+						leaveFrom="opacity-100"
+						leaveTo="opacity-0"
+					>
+						<div className="fixed inset-0 bg-black bg-opacity-25" />
+					</Transition.Child>
+
+					<div className="fixed inset-0 overflow-y-auto">
+						<div className="flex min-h-full items-center justify-center p-4 text-center ">
+							<Transition.Child
+								as={Fragment}
+								enter="ease-out duration-300"
+								enterFrom="opacity-0 scale-95"
+								enterTo="opacity-100 scale-100"
+								leave="ease-in duration-200"
+								leaveFrom="opacity-100 scale-100"
+								leaveTo="opacity-0 scale-95"
+							>
+								<Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-black  p-6 text-left align-middle shadow-xl transition-all">
+									<Dialog.Title
+										as="h3"
+										className="text-lg font-medium leading-6 text-white"
+									>
+										Thank You for Getting in Touch!
+									</Dialog.Title>
+									<div className="mt-2">
+										<p className="text-xs text-gray-300 text-justify">
+											Thank you for getting in touch with
+											me. Your message has been received
+											and I appreciate the opportunity to
+											connect with you. I will respond to
+											your message as soon as possible,
+											and if there is anything specific
+											you would like me to address, please
+											let me know. I look forward to
+											speaking with you soon.
+											<br />
+											<br />
+											Best regards,
+											<br /> Thabish
+										</p>
+									</div>
+
+									<div className="mt-4">
+										<button
+											type="button"
+											className="inline-flex justify-center rounded-md border border-transparent bg-tertiary px-4 py-2 text-sm font-medium text-white hover:bg-white hover:text-black focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 duration-500 transition-colors"
+											onClick={closeModal}
+										>
+											Got it, thanks!
+										</button>
+									</div>
+								</Dialog.Panel>
+							</Transition.Child>
+						</div>
+					</div>
+				</Dialog>
+			</Transition>
 		</div>
 	);
 };
