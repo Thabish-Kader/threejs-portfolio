17e3e01b2963a63124d171a777e5b04c0cad96a3
Thabish <kadertabish2@gmail.com>
2023-03-09 18:45:40 +0400

    layout-smallscreen-added
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- src/components/canvas/AvatarCanvas.tsx
+++ src/components/canvas/AvatarCanvas.tsx
@@ -12,56 +12,38 @@ import { easing } from "maath";
 import MyLoader from "../MyLoader";
 import { ClickMe } from "../ClickMe";
 
-const Avatar = () => {
-	const [index, setIndex] = useState(2);
+const Avatar = ({ smallScreen }: { smallScreen: boolean }) => {
+	const [index, setIndex] = useState(1);
 	const { scene, nodes, animations } = useGLTF("/avatar.glb") as any;
 	const [hovered, setHovered] = useState(false);
 
-	const avatar = useGLTF("/avatar.glb") as any;
+	const avatar = useGLTF("/myavatar.glb") as any;
 	const { actions, names } = useAnimations(avatar.animations, avatar.scene);
-	const halo = useRef<THREE.Mesh>(null!);
+
 	useEffect(() => {
-		// Reset and fade in animation after an index has been changed
 		actions[names[index]]?.reset().fadeIn(0.5).play();
-		// In the clean-up phase, fade it out
+
 		return () => {
 			actions[names[index]]?.fadeOut(0.5);
 		};
 	}, [index, actions, names]);
-	// useFrame((state, delta) => {
-	// 	// Animate the selection halo
-	// 	easing.damp3(halo.current.scale, hovered ? 1.15 : 1, 0.2, delta);
-	// 	easing.dampC(
-	// 		// @ts-ignore
-	// 		halo.current?.material.color,
-	// 		hovered ? "green" : "red",
-	// 		0.5,
-	// 		delta
-	// 	);
-	// });
+
 	return (
 		<group>
 			<primitive
 				onPointerOver={() => console.log("hover")}
 				object={avatar.scene}
-				scale={2}
+				scale={smallScreen ? 1.7 : 2}
 				position-y={-2}
 				rotation-y={-0.5}
+				position-x={[-1]}
 			/>
 
-			{/* <mesh
-				ref={halo}
-				position-x={-3}
-				rotation-y={0.25}
-				onClick={() => setIndex((index + 1) % names.length)}
-				onPointerOver={() => setHovered(true)}
-				onPointerOut={() => setHovered(false)}
+			<Html
+				position={[smallScreen ? -2 : -3.7, smallScreen ? 0 : 0.3, 0]}
 			>
-				<circleGeometry args={[1, 64]} />;
-			</mesh> */}
-			<Html position={[-3.7, 0.3, 0]}>
 				<button
-					className="bg-purple-500 text-black p-2 rounded-lg text-lg w-[200px] hover:bg-white hover:scale-110 duration-500"
+					className="bg-purple-500 text-black w-[100px] p-2  rounded-lg text-xs sm:text-lg sm:w-[200px] hover:bg-white hover:scale-110 duration-500"
 					onClick={() => setIndex((index + 1) % names.length)}
 				>
 					Impress Me!
@@ -72,13 +54,28 @@ const Avatar = () => {
 };
 
 export const AvatarCanvas = () => {
+	const [smallScreen, setSmallScreen] = useState(false);
+
+	useEffect(() => {
+		const mediaQuery = window.matchMedia("(max-width:500px)");
+		setSmallScreen(mediaQuery.matches);
+
+		const handleMediaQueryChange = (e: MediaQueryListEvent) => {
+			setSmallScreen(e.matches);
+		};
+		mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+		return () =>
+			mediaQuery.removeEventListener("change", handleMediaQueryChange);
+	}, []);
+
 	return (
 		<Canvas>
 			<ambientLight intensity={0.5} />
 			<pointLight position={[1, 1, 1]} />
 			<OrbitControls enableZoom={false} />
 			<Suspense fallback={<MyLoader />}>
-				<Avatar />
+				<Avatar smallScreen={smallScreen} />
 			</Suspense>
 		</Canvas>
 	);
